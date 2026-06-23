a55117bd4e025420da541bb8ee0e68f90bb97f48
woheller69 <woheller69@t-online.de>
2026-06-23 09:37:51 +0200

    V3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.woheller69.ttsengine"
         minSdk = 29
         targetSdk = 35
-        versionCode = 32
-        versionName = "3.2"
+        versionCode = 33
+        versionName = "3.3"
 
         vectorDrawables {
             useSupportLibrary = true
